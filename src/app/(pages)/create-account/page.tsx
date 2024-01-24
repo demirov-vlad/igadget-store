3a4aae9b5daded1c851f0add3c7390a9016b298e
Vlad <demirov.vlad@gmail.com>
2024-01-24 14:55:16 +0200

    bug fixes and app completion

--- src/app/(pages)/create-account/page.tsx
+++ src/app/(pages)/create-account/page.tsx
@@ -1,16 +1,14 @@
 import React from 'react'
 import { Metadata } from 'next'
+import Image from 'next/image'
+import Link from 'next/link'
 
-import { Gutter } from '../../_components/Gutter'
 import { RenderParams } from '../../_components/RenderParams'
 import { getMeUser } from '../../_utilities/getMeUser'
 import { mergeOpenGraph } from '../../_utilities/mergeOpenGraph'
 import CreateAccountForm from './CreateAccountForm'
 
 import classes from './index.module.scss'
-import Link from 'next/link'
-import Image from 'next/image'
-import LoginForm from '../login/LoginForm'
 
 export default async function CreateAccount() {
   await getMeUser({
